6d337bfdabb79d531c0d40478e20f87aa0f263a4
MENHOUR Merzouk <merzouk@gmail.com>
2016-09-11 13:16:04 +0200

    email expression verify

--- persist/src/main/java/com/org/tools/Utils.java
+++ persist/src/main/java/com/org/tools/Utils.java
@@ -41,9 +41,9 @@ public class Utils
     */
    public static boolean isValidEmailAddress( String email )
    {
-      String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-      java.util.regex.Pattern p = java.util.regex.Pattern.compile( ePattern );
-      java.util.regex.Matcher m = p.matcher( email );
-      return m.matches();
+      String emailPatternExpression = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+      java.util.regex.Pattern pattern = java.util.regex.Pattern.compile( emailPatternExpression );
+      java.util.regex.Matcher matcher = pattern.matcher( email );
+      return matcher.matches();
    }
 }
